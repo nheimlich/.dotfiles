dfc38c3197a73890853cb35fde33bee7c16ea9e1
Nolan Heimlich <76967780+nheimlich@users.noreply.github.com>
2025-04-03 23:06:35 -0400

    fix(wez): no tabs please
    Signed-off-by: Nolan Heimlich <76967780+nheimlich@users.noreply.github.com>

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,15 +5,24 @@ config = wezterm.config_builder()
 
 config = {
   automatically_reload_config = true,
-  enable_tab_bar = true,
+  enable_tab_bar = false,
   use_fancy_tab_bar = true,
+  show_close_tab_button_in_tabs = false,
+  show_tab_index_in_tab_bar = false,
   window_close_confirmation = "NeverPrompt",
   window_decorations = "RESIZE",
-  window_background_opacity = 0.75,
+  window_background_opacity = 0.65,
+  text_background_opacity = 0.65,
   default_cursor_style = "SteadyUnderline",
   color_scheme = "iceberg-dark",
   font = wezterm.font("JetBrains Mono"),
   keys = {
+    -- Toggle tab bar with CMD + Shift + T
+    {
+      key = "T",
+      mods = "CMD|SHIFT",
+      action = wezterm.action.EmitEvent("toggle-tabs"),
+    },
     -- Switch to the previous tab with Cmd + Shift + Left
     {
       key = "LeftArrow",
@@ -28,5 +37,17 @@ config = {
     },
   },
 }
+
+wezterm.on("toggle-tabs", function(window, _)
+  local overrides = window:get_config_overrides() or {}
+  if overrides.enable_tab_bar == false then
+    wezterm.log_info("tab bar shown")
+    overrides.enable_tab_bar = true
+  else
+    wezterm.log_info("tab bar hidden")
+    overrides.enable_tab_bar = false
+  end
+  window:set_config_overrides(overrides)
+end)
 
 return config
